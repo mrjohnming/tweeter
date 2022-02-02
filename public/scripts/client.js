017cc68b7ab0a6ae7f8486a5a66f2bc464f64519
mrjohnming <john.ngai@hotmail.com>
2022-02-02 21:59:35 +0000

    Implement a loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $( document ).ready(function() {
   
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = tweetData => {
     const $tweet = `
       <div class="tweet">
@@ -62,12 +35,24 @@ $( document ).ready(function() {
       }
     }
     
-  renderTweets(data);
+    
+    // Form submission using JQuery.
+    $( '.tweet-form' ).submit(function( event ) {
+      event.preventDefault() 
+      const payload = $( this ).serialize();
+      $.post('/tweets', payload);      
+    });
+    
+    // Fetch tweets with Ajax.
+    const loadTweets = () => {
+      $.ajax('/tweets', { method: 'GET' })
+      .then(function(data) {
+        console.log('data =', data);
+        renderTweets(data);
+      });
+    };
+    
+    loadTweets();
 
-  // Form submission using JQuery.
-  $( '.tweet-form' ).submit(function( event ) {
-    event.preventDefault() 
-    const payload = $( this ).serialize();
-    $.post('/tweets', payload);
   });
-});
+
