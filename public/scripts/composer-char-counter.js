4466d3f0b5c53667daf9339be29b049138d6e88c
mrjohnming <john.ngai@hotmail.com>
2022-02-04 04:56:00 +0000

    Change the counter text to red when over the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
-  // console.log("jQuery is ready!");
-
   $( '#tweet-text' ).on('input', function(event) {
     const max = 140;
     const counter = this.value.length;
     const remainder = max - counter;
     const tweetChar = $( '#tweet-text' ).parent().children('.tweet-footer').children('.tweet-char');
+    
+    // The character counter will change from the default (inherited) colour to red when >140 characters.
+    remainder < 0 ? tweetChar.css('color', 'red') : tweetChar.css('color', 'inherit');
+    
+    // Update the character counter each time the length of characters has changed.
     tweetChar.val(remainder);
   });
-
 });
